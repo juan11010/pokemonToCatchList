1c850ed8c1813c4d6d936d269cf865477dc725a2
Juan Contreras <juan26_2000@live.com.mx>
2023-08-28 10:52:42 -0600

    Update project structure

---
+++ src/components/NewForm.jsx
@@ -0,0 +1,12 @@
+function NewForm() {
+  return (
+    <>
+      <form>
+        <label htmlFor="pokemon">New Pokemon</label>
+        <input type="text" id="pokemon" />
+      </form>
+    </>
+  );
+}
+
+export default NewForm;
