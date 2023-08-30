a0716dc5786c8a9d02b4a27da1a5b2e9f9622c88
Juan Contreras <juan26_2000@live.com.mx>
2023-08-30 16:10:40 -0600

    Create pokemon display card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,26 @@
-import { Card, CardSection } from "@mantine/core";
+import { Card, Image, Text, Group } from "@mantine/core";
 
-const PokemonCard = ({data}) => {
-  return <Card>
-    <CardSection>
-    </CardSection>
-  </Card>;
-};
+function PokemonCard({ pokemonData }) {
+  return (
+    <Card shadow="lg" padding="lg" radius="md" withBorder my="md">
+      <Card.Section>
+        <Image
+          src={pokemonData.sprites.front_default}
+          height={160}
+          alt={pokemonData.name}
+        />
+      </Card.Section>
+
+      <Group position="center" mt="md" mb="xs">
+        <Text weight={500}>{pokemonData.name}</Text>
+      </Group>
+
+      <Text size="sm" color="dimmed">
+        height: {pokemonData.height}
+        weight: {pokemonData.weight}
+      </Text>
+    </Card>
+  );
+}
 
 export default PokemonCard;
