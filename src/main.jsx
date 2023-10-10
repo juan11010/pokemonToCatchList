36582e282a0192de78cf4c3d4145470e6a86592a
Juan Contreras <juan26_2000@live.com.mx>
2023-10-10 16:21:19 -0600

    change theme

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MantineProvider
-      theme={{ colorScheme: "dark" }}
+      theme={{ colorScheme: "light" }}
       withGlobalStyles
       withNormalizeCSS
     >
